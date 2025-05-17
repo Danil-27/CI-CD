c57e1c50b4dc5eb3420ba1339869b177169aa5a5
Danil-27 <pde200126@gmail.com>
2025-05-17 18:53:09 +0300

    Done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,9 +8,9 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      CХахаха победа Убраааааааали пробелы ssssss 3
+      Любовь победит зло
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3333333333</a>. What's next?
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
   </div>
 </template>
